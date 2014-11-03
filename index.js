48c5c2d627d40dbd606229f666ab803a53db105b
Mr_Mig <alexey.migutsky@gmail.com>
2014-11-03 04:53:43 +0100

    Updated am.conventions to 1.0.3
    Update logic to use am.convention v1.0.3 names

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ var conv = require('am.convention');
 module.exports = stateFactory;
 
 function stateFactory(componentType, definition, obj) {
-  var prefix = conv.prefix(definition.project);
-  var moduleName = conv.ngModuleName(prefix.lower, componentType, definition.name);
-  var stateName = conv.composedName(prefix.upper, definition.name, conv.behaviourComponents.state);
+  var moduleName = conv.names.ngModule(componentType, definition.name);
+  var stateName = conv.names.state(definition.name);
 
   return {
     moduleName: moduleName,
